ae67c8de54058fb1bf5f52a713d3295915231d4b
Ramin <r.ramazanpour@aut.ac.ir>
2023-11-17 21:14:11 +0300

    add styled components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 'use client';
 import styles from './page.module.css';
 import { useWeb3Modal } from '@web3modal/wagmi/react';
+import styled from 'styled-components';
 
 export default function Home() {
 	const { open: openConnectModal } = useWeb3Modal();
 	return (
 		<main className={styles.main}>
-			<div>
-				<button onClick={() => openConnectModal && openConnectModal()}>
+			<Wrapper>
+				<Button onClick={() => openConnectModal && openConnectModal()}>
 					Connect Wallet
-				</button>
-			</div>
+				</Button>
+			</Wrapper>
 		</main>
 	);
 }
+
+const Button = styled.button`
+	padding: 10px 20px;
+	cursor: pointer;
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 500px;
+`;
--- src/app/ramin/page.tsx
+++ src/app/ramin/page.tsx
@@ -1,5 +1,12 @@
 'use client';
-import Button from '@/components/Button';
+import styled from 'styled-components';
 export default function App() {
-	return <Button />;
+	return <Wrapper>It's Ramin route</Wrapper>;
 }
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 500px;
+`;
