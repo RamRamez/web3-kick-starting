409669c979d3c601ae2a028e22745dbb22aadb93
Ramin <r.ramazanpour@aut.ac.ir>
2023-11-17 21:16:16 +0300

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import { ReactNode } from 'react';
-import './globals.css';
 import { Web3Modal } from '@/context/Web3Modal';
+import Header from '@/components/Header';
+import './globals.css';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en'>
 			<body>
-				<Web3Modal>{children}</Web3Modal>
+				<Web3Modal>
+					<Header />
+					{children}
+				</Web3Modal>
 			</body>
 		</html>
 	);
